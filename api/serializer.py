20bc00937edffc70fc7c7b453e4996baa44dc0a7
Alena Nidilko <alenkan96@gmail.com>
2022-07-03 23:12:15 +0300

    Change serializerls

--- api/serializer.py
+++ api/serializer.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from .models import Questionnaire, Question
 
 
+class QuestionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Question
+        fields = ('id', 'question', 'correct_answers', 'incorrect_answers')
+
+
 class QuestionnaireSerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(read_only=True, many=True)
+
     class Meta:
         model = Questionnaire
-        fields = '__all__'
+        fields = ('questionnaire_name', 'questions',)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.generics import (ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -11,8 +11,6 @@ from . import serializer
 class QuestionAPI(ListCreateAPIView):
     queryset = Questionnaire.objects.all()
     serializer_class = serializer.QuestionnaireSerializer
-    # permission_classes = (AllowAny,)
-    # permission_classes = (IsAuthenticated)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -35,30 +33,6 @@ class QuestionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 def list_questionnaire(request):
     questionnaires = Questionnaire.objects.all()
     return render(request, 'list_questionnaire.html', {'questionnaire': questionnaires})
-    """
-    Функция отображения для домашней страницы сайта.
-    
-    # Генерация "количеств" некоторых главных объектов
-    num_questionnaire = Questionnaire.objects.all().count()
-    # num_instances = BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
-    # num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-
-    num_authors = Author.objects.count()  # The 'all()' is implied by default.
-
-    # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
-
-    # Render the HTML template index.html with the data in the context variable.
-    return render(
-        request,
-        'index.html',
-        context={'num_books': num_books, 'num_instances': num_instances,
-                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
-                 'num_visits': num_visits},  # num_visits appended
-    )
-    """
 
 
 def index(request):
